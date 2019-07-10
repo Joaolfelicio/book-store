9d3a3a30c6fd0a782786011579df282c271498ae
nasser ninja <alkar0abdulnasser@gmail.com>
2019-07-10 11:22:02 +0200

    finishing thr navbar

---
+++ navbar.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang=''>
+<head>
+   <meta charset='utf-8'>
+   <meta http-equiv="X-UA-Compatible" content="IE=edge">
+   <meta name="viewport" content="width=device-width, initial-scale=1">
+   <link rel="stylesheet" href="styles.css">
+   <script src="http://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+   <script src="script.js"></script>
+   <title>Joao</title>
+
+</head>
+<body>
+
+<div id='cssmenu'>
+<ul>
+   <li class='active'><a href='index.php'>Home</a></li>
+   <li><a href='products.php'>Products</a></li>
+   <li><a href='#'>Company</a></li>
+   <li><a href='#'>Contact</a></li>
+   <li><a href='contact_page.php'>Contact</a></li>
+</ul>
+</div>
+    
+</body>
+</html>
